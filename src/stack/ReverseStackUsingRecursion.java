9b39ede77912c58a106256a0078d239aad2bd7b2
gaurav <gaurav10610@gmail.com>
2021-03-28 13:06:21 +0530

    Reverse a stack using recursion

---
+++ src/stack/ReverseStackUsingRecursion.java
@@ -0,0 +1,58 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * 
+ * Write a recursive logic to reverse the stack.You are not allowed to use loop constructs like
+ * while, for..etc, and you can only use the following ADT functions on Stack S:
+ * 
+ * isEmpty(S)
+ * 
+ * push(S)
+ * 
+ * pop(S)
+ *
+ */
+public class ReverseStackUsingRecursion {
+
+  public static void main(String[] args) {
+
+    Stack<Integer> stack = new Stack<>();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+
+    System.out.println("Original stack: ");
+    stack.forEach(value -> System.out.print(value + " "));
+    System.out.println("\n");
+
+
+    System.out.println("Reversed stack: ");
+    while (!stack.isEmpty()) {
+      System.out.print(stack.pop() + " ");
+    }
+  }
+
+  public static void reverseStack(Stack<Integer> stack) {
+    if (!stack.isEmpty()) {
+      int poppedElement = stack.pop();
+      reverseStack(stack);
+      insertAtBottom(poppedElement, stack);
+    }
+  }
+
+  public static void insertAtBottom(int x, Stack<Integer> stack) {
+
+    if (stack.isEmpty()) {
+      stack.push(x);
+    } else {
+      int poppedElement = stack.pop();
+      insertAtBottom(x, stack);
+      stack.push(poppedElement);
+    }
+  }
+
+}
