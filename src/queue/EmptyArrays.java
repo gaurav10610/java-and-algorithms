3659dcc797ec2486358fd5d701915349f0e1d060
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2022-09-17 11:28:24 +0530

    Added documentation in empty arrays problem
    Signed-off-by: Gaurav Kumar Yadav <gaurav10610@gmail.com>

--- src/queue/EmptyArrays.java
+++ src/queue/EmptyArrays.java
@@ -7,11 +7,33 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * You are given two arrays each of size , and consisting of the first positive integers each
+ * exactly once, that is, they are permutations.
+ * 
+ * Your task is to find the minimum time required to make both the arrays empty. The following two
+ * types of operations can be performed any number of times each taking 1 second:
+ * 
+ * 1. In the first operation, you are allowed to rotate the first array clockwise.
+ * 
+ * 2. In the second operation, when the first element of both the arrays is the same, they are
+ * removed from both the arrays and the process continues.
+ * 
+ * Example -
+ * 
+ * Input -
+ * 
+ * Array1 - 1 3 2
+ * 
+ * Array2 - 2 3 1
+ *
+ * Output - 6
+ */
 public class EmptyArrays {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    Integer.parseInt(br.readLine());
+    br.readLine();
     int totalTime = 0;
 
     String[] a = br.readLine().split(" ");
