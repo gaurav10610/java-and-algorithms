6306e406fa104731cccf08a149cb7708939b0c1c
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2022-09-24 12:05:11 +0530

    optional demo

--- src/streams/FlatMapExample.java
+++ src/streams/FlatMapExample.java
@@ -1,5 +1,6 @@
 package streams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,17 +8,16 @@ public class FlatMapExample {
 
   public static void main(String[] args) {
 
-    List<Integer> row1 = Arrays.asList(1, 2, 3, 4, 5);
-    List<Integer> row2 = Arrays.asList(1, 2, 3, 4, 5);
-    List<Integer> row3 = Arrays.asList(1, 2, 3, 4, 5);
-    List<Integer> row4 = Arrays.asList(1, 2, 3, 4, 5);
-    List<Integer> row5 = Arrays.asList(1, 2, 3, 4, 5);
+    List<List<Integer>> listOfList = new ArrayList<>();
+    int i = 5;
+    while (i >= 0) {
+      listOfList.add(Arrays.asList(1, 2, 3, 4, 5));
+      i++;
+    }
 
-    List<List<Integer>> listOfList = Arrays.asList(row1, row2, row3, row4, row5);
-
-    Integer sum = listOfList.stream().flatMap(list -> list.stream())
+    Integer sum = listOfList.stream().flatMap(List::stream)
         .reduce((number1, number2) -> number1 + number2).get();
-    
+
     System.out.println(sum);
   }
 
